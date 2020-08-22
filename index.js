91e2653c702d838a34fce606fac9adde1821e4ad
Enrique Lopez <elop22@gmail.com>
2020-08-22 13:39:38 -0600

    added user questions + functions

--- index.js
+++ index.js
@@ -1,14 +1,85 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require('./utils/generateMarkdown')
 
-];
+// array of questions for user
+function promptUser(){
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "what is your GitHub username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "what is your email address?",
+    },
+    {
+      type: "input",
+      name: "project name",
+      message: "what is your project's name?",
+    },
+    {
+      type: "input",
+      name: "descpription",
+      message: "Please write a short description of your project",
+    },
+    {
+      type: "list",
+      name: "License",
+      message: "What kind of license should your project have?",
+      choices: [
+        'MIT',
+        'GNU',
+        'ISC',
+        'Apache',
+        'Unlicensed'
+      ]
+    },
+    {
+      type: "input",
+      name: "dependencies",
+      message: "what command should be run to install dependencies?",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "What command should be run to run test?",
+    },
+    {
+      type: "input",
+      name: "info",
+      message: "What does the user need to know about using the repo?",
+    },
+    {
+      type: "input",
+      name: "info",
+      message: "What does the user need to know about contrinuting to the repo?",
+    }
+  ])
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile("README.md", data, function(err) {
+    if(err){
+      return console.log(err);
+    }
+  })
 }
 
 // function to initialize program
-function init() {
+async function init() {
+  try{
+    const answers = await promptUser();
+    const markdown = generateMarkdown(answers);
+
+    await writeFileAsync("README.md", markdown);
+  }catch(err){
+    console.log(err);
+  }
 
 }
 
